633a572c8d40f578262928a60d41d11a1b7e833e
mouseorcleg <torindubozit@gmail.com>
2023-03-22 18:38:25 +0000

    added test and controller

---
+++ CalculatorUIKit/CalculatorController.swift
@@ -0,0 +1,26 @@
+//
+//  CalculatorController.swift
+//  CalculatorUIKit
+//
+//  Created by Maria Kharybina on 22/03/2023.
+//
+
+import Foundation
+
+struct CalculatorController {
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    func subtract(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    func multiply(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    func divide(a: Int, b: Int) -> Int {
+        return a / b
+    }
+}
--- CalculatorUIKitTests/CalculatorUIKitTests.swift
+++ CalculatorUIKitTests/CalculatorUIKitTests.swift
@@ -8,29 +8,49 @@
 import XCTest
 @testable import CalculatorUIKit
 
-final class CalculatorUIKitTests: XCTestCase {
+final class CalculatorControllerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAddition() throws {
+        //Given - controller, two numbers
+        let testSubject = CalculatorController()
+        let numberOne: Int = 5
+        let numberTwo: Int = -10
+        
+        //When - add two numbers using a controller
+        let result = testSubject.add(a: numberOne, b:numberTwo)
+        
+        //Then - one number as a result
+        XCTAssertEqual(result, numberOne + numberTwo)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testSubstacting() throws {
+        let testSubject = CalculatorController()
+        let numberOne: Int = 5
+        let numberTwo: Int = -10
+        
+        let result = testSubject.subtract(a: numberOne, b: numberTwo)
+        
+        XCTAssertEqual(result, numberOne - numberTwo)
+        
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testMultiplying() throws {
+        let testSubject = CalculatorController()
+        let numberOne: Int = 5
+        let numberTwo: Int = -10
+        
+        let result = testSubject.multiply(a: numberOne, b: numberTwo)
+        
+        XCTAssertEqual(result, numberOne * numberTwo)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDivision() throws {
+        let testSubject = CalculatorController()
+        let numberOne: Int = 5
+        let numberTwo: Int = -10
+        
+        let result = testSubject.divide(a: numberOne, b: numberTwo)
+        
+        XCTAssertEqual(result, numberOne / numberTwo)
     }
-
 }
